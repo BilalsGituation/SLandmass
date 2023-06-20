267ad78b36be2c4dbb9e94b81a288fe09e68355c
BilalsGituation <billyasser@hotmail.co.uk>
2023-06-20 18:51:21 +0200

    onValidate()d some params to useful ranges

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,22 +1,48 @@
 using UnityEngine;
 using System.Collections;
 
-public class MapGenerator : MonoBehaviour {
+public class MapGenerator : MonoBehaviour
+{
     // map properties
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
 
+    [Range(0, 10)]
     public int octaves;
+    //[Range(0,1)]
     public float persistence;
     public float lacunarity;
 
+    public int seed;
+    public Vector2 offset;
+
     public bool autoUpdate;
 
-    public void GenerateMap() {
-        float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale, octaves, persistence, lacunarity);
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistence, lacunarity, offset);
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
         display.DrawNoiseMap(noiseMap);
+    }
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
 }
